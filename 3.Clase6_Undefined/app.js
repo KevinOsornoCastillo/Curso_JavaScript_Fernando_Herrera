7fadedd4a1a0583f266ad221dffafbbebfb7ae4d
Kevin Osorno Castillo <kevinosornocastillo@gmail.com>
2021-06-17 12:54:11 -0500

    Inicio de curso, clase 1 hasta la 7

---
+++ 3.Clase6_Undefined/app.js
@@ -0,0 +1,22 @@
+var a = 1;
+var b = "Kevin Osorno";
+
+/*
+Con alert se detiene la ejecución del programa hasta que le demos
+aceptar en la ventana emergente.
+*/
+//alert(a);
+
+/*
+La forma correcta de hacer anotaciones en el consola es usando console esto
+sirve para hacer "debug" y ver el valor de una variable.
+*/
+console.log(a);
+
+/*Hay diferente métodos que podemos usar con el objeto console*/
+console.warn(a);
+console.error(a);
+console.info(a);
+
+/*También podemos hacer operaciones mátematicas*/
+console.log(a+1);
